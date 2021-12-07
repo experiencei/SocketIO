ae3c1f77a453f26ff09ad608d1390201df053b05
experiencei <ayelojahighbee01@gmail.com>
2021-12-07 13:13:31 +0100

    working with socket.io

--- javascripts/script.js
+++ javascripts/script.js
@@ -202,3 +202,9 @@ socket.on('startGame', (refereeId) => {
   isReferee = socket.id === refereeId;
   startGame();
 });
+
+socket.on('paddleMove', (paddleData) => {
+  // Toggle 1 into 0, and 0 into 1
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
+});
