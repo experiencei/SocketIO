b625273c4bdeb2c27c6a5855b66e8d638aba119b
experiencei <ayelojahighbee01@gmail.com>
2021-12-07 13:18:45 +0100

    working with socket.io

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ io.on('connection', (socket) => {
     }
 });
 
-socket.on('paddleMove', (paddleData) => {
+  socket.on('paddleMove', (paddleData) => {
     socket.broadcast.emit('paddleMove', paddleData);
+  });
+
+  socket.on('ballMove', (ballData) => {
+    socket.broadcast.emit('ballMove', ballData);
   });
 })
